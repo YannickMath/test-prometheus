ab54c9f0b31c8d02424770d6b3e4e8da83ae0ec1
Yannick.Mathieu <yannick.mathieu@toolib.fr>
2023-08-25 10:48:55 +0200

    add gem prometheus

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module TestPrometheus
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use PrometheusExporter::Middleware
+
   end
 end
